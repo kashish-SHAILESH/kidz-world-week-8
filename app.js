778313401d02797d7ffca116b0665fa98685a855
kashish-SHAILESH <75208356+kashish-SHAILESH@users.noreply.github.com>
2021-01-29 19:09:53 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var cartvalue =document.getElementById("cart-value")
+var cartbutton=document.getElementById("cart")
+
+var  book1add=document.getElementById("book1")
+var  book2add=document.getElementById("book2")
+var  book3add=document.getElementById("book3")
+var  book4add=document.getElementById("book4")
+var  book5add=document.getElementById("book5")
+var  book6add=document.getElementById("book6")
+
+var  game1add=document.getElementById("game1")
+var  game2add=document.getElementById("game2")
+var  game3add=document.getElementById("game3")
+var  gmae4add=document.getElementById("game4")
+
+var  craft1add=document.getElementById("craft1")
+var  craft2add=document.getElementById("craft2")
+var  craft3add=document.getElementById("craft3")
+var  craft4add=document.getElementById("craft4")
+
+console.log(book1add)
